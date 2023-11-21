cdc614fa9ba8efa62d3df69eba2ce444ff48fcb7
Prem Udeshi <premudeshi99@gmail.com>
2023-11-21 16:01:56 -0500

    Added save and import function

--- app/setup.py
+++ app/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Postal API Wrapper for Python'
 LONG_DESCRIPTION = 'A package that introduces Postal API for Python.'
 
